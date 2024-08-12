5a2e558ec503883f750cca16695f9d3c6377ec63
EggsyOnCode <farzansaqib2003@gmail.com>
2024-08-13 01:53:14 +0500

    found the bug via invariant test

---
+++ test/invariant_break/Invariant.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import { Test, StdInvariant, console2 } from "forge-std/Test.sol";
+import { PoolFactory } from "src/PoolFactory.sol";
+import { TSwapPool } from "src/TSwapPool.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import { TSwapPoolHandler } from "./Handler.t.sol";
+
+contract Invariant is StdInvariant, Test {
+    PoolFactory factory;
+    TSwapPool pool;
+    ERC20Mock poolToken;
+    ERC20Mock weth;
+    ERC20Mock tokenB;
+
+    int256 constant STARTING_X = 100e18; // starting ERC20
+    int256 constant STARTING_Y = 50e18; // starting WETH
+    uint256 constant FEE = 997e15; //
+    int256 constant MATH_PRECISION = 1e18;
+
+    TSwapPoolHandler handler;
+
+    function setUp() public {
+        weth = new ERC20Mock();
+        poolToken = new ERC20Mock();
+        factory = new PoolFactory(address(weth));
+        pool = TSwapPool(factory.createPool(address(poolToken)));
+
+        // Create the initial x & y values for the pool
+        poolToken.mint(address(this), uint256(STARTING_X));
+        weth.mint(address(this), uint256(STARTING_Y));
+        poolToken.approve(address(pool), type(uint256).max);
+        weth.approve(address(pool), type(uint256).max);
+        pool.deposit(uint256(STARTING_Y), uint256(STARTING_Y), uint256(STARTING_X), uint64(block.timestamp));
+
+        handler = new TSwapPoolHandler(pool);
+
+        bytes4[] memory selectors = new bytes4[](2);
+        selectors[0] = TSwapPoolHandler.deposit.selector;
+        selectors[1] = TSwapPoolHandler.swapPoolTokenForWethBasedOnOutputWeth.selector;
+
+        targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
+        targetContract(address(handler));
+    }
+
+    function invariant_deltaXFollowsMath() public {
+        assertEq(handler.actualDeltaX(), handler.expectedDeltaX());
+    }
+
+    function invariant_deltaYFollowsMath() public {
+        assertEq(handler.actualDeltaY(), handler.expectedDeltaY());
+    }
+}
